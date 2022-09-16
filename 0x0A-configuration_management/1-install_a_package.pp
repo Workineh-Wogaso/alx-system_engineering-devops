4eab228f7894a1f3a3e432b98fb64cf233a20986
HaxorLeoul <workinehw94@gmail.com>
2022-09-16 14:10:32 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install puppet-lint v2.5.0
-package { 'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
